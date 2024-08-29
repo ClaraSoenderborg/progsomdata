d0d2adc68a77e0141879b81fad53d750a5d8c7be
Clara Sønderborg <auso@itu.dk>
2024-08-29 11:31:02 +0200

    exercise 1.1

--- ProgSomData/intro2.fs
+++ ProgSomData/intro2.fs
@@ -32,8 +32,8 @@ let e2 = Prim("+", CstI 3, Var "a");;
 let e3 = Prim("+", Prim("*", Var "b", CstI 9), Var "a");;
 
 
-(* Evaluation within an environment *)
 
+(* Evaluation within an environment *)
 let rec eval e (env : (string * int) list) : int =
     match e with
     | CstI i            -> i
@@ -49,3 +49,79 @@ let e2v2 = eval e2 [("a", 314)];;
 let e3v  = eval e3 env;;
 
 
+
+
+
+(* 1.1 i*)
+let rec eval1 e (env : (string * int) list) : int =
+    match e with
+    | CstI i            -> i
+    | Var x             -> lookup env x 
+    | Prim("+", e1, e2) -> eval e1 env + eval e2 env
+    | Prim("*", e1, e2) -> eval e1 env * eval e2 env
+    | Prim("-", e1, e2) -> eval e1 env - eval e2 env
+    | Prim("max", e1, e2) ->
+        System.Math.Max(eval e1 env, eval e2 env)
+    | Prim("min", e1, e2) ->
+    System.Math.Min(eval e1 env, eval e2 env)
+    | Prim("==", e1, e2) ->
+        match eval e1 env = eval e2 env with
+        | true -> 1
+        | _ -> 0
+    | Prim _            -> failwith "unknown primitive";;
+    
+    
+(*1.1 ii*)
+let exampleMax = eval1 (Prim("max", e1, e2)) env
+let exampleMin = eval1 (Prim("min", e1, e2)) env
+let exampleEqualTrue = eval1 (Prim("==", CstI 17, e1)) env
+let exampleEqualFalse = eval1 (Prim("==", e1, e2)) env
+
+
+// 1.1 iii
+let eval2 e (env : (string * int) list) : int =
+    match e with
+    | CstI i            -> i
+    | Var x             -> lookup env x
+    | Prim(op, e1,e2) ->
+        let res1 = eval e1 env
+        let res2 = eval e2 env
+        match op with
+        | "+" -> res1 + res2
+        | "-" -> res1 - res2
+        | "*" -> res1 * res2
+        | "max" -> System.Math.Max(res1, res2)
+        | "min" -> System.Math.Min(res1, res2)
+        | "==" -> if res1 = res2 then 1 else 0
+        | _ -> failwith "unknown primitive"
+
+// 1.1 iv
+type expr1 = 
+  | CstI of int
+  | Var of string
+  | Prim of string * expr1 * expr1
+  | If of expr1 * expr1 * expr1
+  
+// 1.1 v
+
+let rec eval3 (e:expr1) (env : (string * int) list) : int =
+    match e with
+    | CstI i            -> i
+    | Var x             -> lookup env x 
+    | Prim("+", e1, e2) -> eval3 e1 env + eval3 e2 env
+    | Prim("*", e1, e2) -> eval3 e1 env * eval3 e2 env
+    | Prim("-", e1, e2) -> eval3 e1 env - eval3 e2 env
+    | Prim("max", e1, e2) ->
+        System.Math.Max(eval3 e1 env, eval3 e2 env)
+    | Prim("min", e1, e2) ->
+    System.Math.Min(eval3 e1 env, eval3 e2 env)
+    | Prim("==", e1, e2) ->
+        if eval3 e1 env = eval3 e2 env then 1 else 0
+    | Prim _            -> failwith "unknown primitive"
+    | If(e1, e2, e3) ->
+        match eval3 e1 env with
+        | x when x > 0 -> eval3 e2 env
+        | _ -> eval3 e3 env
+    
+
+let exampleIfTrue = eval3 (If(Var "a", CstI 11, CstI 22)) env
