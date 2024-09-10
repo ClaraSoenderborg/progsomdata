07201f4aa4fea04e87f25b4feee2b1d2929300d0
Clara Sønderborg <auso@itu.dk>
2024-09-10 21:50:56 +0200

    hellolex Q2

--- ProgSomData/Assignment2/HelloLex/obj/Debug/net8.0/hello.AssemblyInfo.fs
+++ ProgSomData/Assignment2/HelloLex/obj/Debug/net8.0/hello.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("hello")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f0f3093f27edb3c602af742381fac43f4a3219f")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a72cc97fec3ab455f4bf72046ba86ba7e385f9")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("hello")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("hello")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
